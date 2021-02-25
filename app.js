85c42b2a329e59ba15d135cdb757c06d34bf08c5
jmf4nier <jmf4nier@gmail.com>
2021-02-24 18:10:06 -0600

    stuff

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -38,12 +47,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 
 module.exports = app;
